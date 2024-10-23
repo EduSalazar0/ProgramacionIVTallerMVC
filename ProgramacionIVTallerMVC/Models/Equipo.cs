802d54eefc17b38e823e7eb2a7ead6f0bd89424d
EduSalazar0 <edutebansalazar95@gmail.com>
2024-10-23 18:46:13 -0500

    Agregar archivos de proyecto.

---
+++ ProgramacionIVTallerMVC/Models/Equipo.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgramacionIVTallerMVC.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = null!;
+
+        [MaxLength(100)]
+        public string Ciudad { get; set; } = null!;
+
+        [Range(0, 100)]
+        public int Titulos { get; set; }
+
+        public bool AceptaExtranjeros { get; set; }
+
+        [ForeignKey("Estadio")]
+        public int IDEstadio { get; set; }
+
+        public virtual Estadio? Estadio { get; set; } = null!;
+    }
+}
---
+++ ProgramacionIVTallerMVC/Models/Jugador.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgramacionIVTallerMVC.Models
+{
+    public class Jugador
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = null!;
+
+        [Required]
+        [StringLength(50)]
+        public string Posicion { get; set; } = null!;
+
+        [Range(0, 100)]
+        public int Edad { get; set; }
+
+        [ForeignKey("Equipo")]
+        public int IDEquipo { get; set; }
+
+        public virtual Equipo? Equipo { get; set; }
+    }
+}
